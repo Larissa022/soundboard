739967acec9b21e23094f94dfcfb09e2e9d09df0
Larissa Nascimento <113188584+Larissa022@users.noreply.github.com>
2024-04-28 22:21:58 -0300

    Update script.js
    Corrigi o caminho dos audios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let audios = [
-    {caminho:'/assets/audios/bom-dia-princesa.mp3', legenda:'bom dia princesa'},
-    {caminho:'/assets/audios/cade_menininho_de_papai.mp3', legenda:'minino de papai'},
-    {caminho:'/assets/audios/eae-mane-cade-o-pix-mwahahaha.mp3', legenda:'cade o pix'},
-    {caminho:'/assets/audios/elon-musk-uma-vez-disse-estourado.mp3', legenda:'elon musk'},
-    {caminho:'/assets/audios/ja-ganhou-tantantan.mp3', legenda:'ja ganhou tan tan tan'},
-    {caminho:'/assets/audios/prefeito-chuva.mp3', legenda:'obrigado meu prefeito'},
-    {caminho:'/assets/audios/sem-h-calanguinho.mp3', legenda:'calanguinho'},
+    {caminho:'assets/audios/bom-dia-princesa.mp3', legenda:'bom dia princesa'},
+    {caminho:'assets/audios/cade_menininho_de_papai.mp3', legenda:'minino de papai'},
+    {caminho:'assets/audios/eae-mane-cade-o-pix-mwahahaha.mp3', legenda:'cade o pix'},
+    {caminho:'assets/audios/elon-musk-uma-vez-disse-estourado.mp3', legenda:'elon musk'},
+    {caminho:'assets/audios/ja-ganhou-tantantan.mp3', legenda:'ja ganhou tan tan tan'},
+    {caminho:'assets/audios/prefeito-chuva.mp3', legenda:'obrigado meu prefeito'},
+    {caminho:'assets/audios/sem-h-calanguinho.mp3', legenda:'calanguinho'},
     {caminho:'assets/audios/lobo-pidao.mp3', legenda:'lobo'},
-    {caminho:'/assets/audios/voce-era-o-meu-duo.mp3', legenda:'vc era meu duo'},
+    {caminho:'assets/audios/voce-era-o-meu-duo.mp3', legenda:'vc era meu duo'},
 ];
 
 let botoes = document.querySelectorAll('.botao');
